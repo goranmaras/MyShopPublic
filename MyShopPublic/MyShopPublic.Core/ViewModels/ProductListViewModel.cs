223a615b38f56e5b8abc8cde2933ed3d256a0c07
Goran <goran.maras77@gmail.com>
2020-09-29 15:52:52 +0200

    Filtering of Products and styling it

---
+++ MyShopPublic/MyShopPublic.Core/ViewModels/ProductListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MyShopPublic.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShopPublic.Core.ViewModels
+{
+    public class ProductListViewModel
+    {
+        public IEnumerable<Product> Product { get; set; }
+        public IEnumerable<ProductCategory> ProductCategories { get; set; }
+    }
+}
