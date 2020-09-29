145bb7c42a24989cea8471eb048a96dc2da3339c
Goran <goran.maras77@gmail.com>
2020-09-29 13:51:54 +0200

    Database setup and migrations

---
+++ MyShopPublic/MyShopPublic.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using MyShopPublic.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShopPublic.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories {get;set;}
+    }
+}
